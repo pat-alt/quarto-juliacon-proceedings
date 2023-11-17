8df02e09a91ecb84a71c754cdf8c138fde0dfe7f
pat-alt <altmeyerpat@gmail.com>
2023-11-17 09:10:22 +0100

    this is not working

--- _extensions/juliacon-proceedings/juliacon-proceedings.lua
+++ _extensions/juliacon-proceedings/juliacon-proceedings.lua
@@ -1,1 +1,12 @@
--- TODO: Transform the document using a filter, if needed
+-- Filter to apply styling to Julia code chunks.
+local function CodeBlock (elem)
+    if elem.c[1][2][1] == "julia" then
+        return pandoc.RawBlock("latex", "\n\\begin{JuliaCode}\n"..elem.text.."\n\\end{JuliaCode}\n")
+    else
+        return elem
+    end
+end
+
+return { { CodeBlock = CodeBlock } }
+
+
